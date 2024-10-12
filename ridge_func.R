6bc81f2738d7d8b93437ca1311b945233655a9a5
shleung101 <shleung@ucdavis.edu>
2024-10-11 21:30:20 -0400

    finished Q1

--- ridge_func.R
+++ ridge_func.R
@@ -34,6 +34,8 @@ training <- function(true_beta, var_cov, sigma2, lambda, ntrain){
       return(NA)}    )
   
   
+  
+  
   #### Draw training samples #####
   set.seed(69)
   
@@ -45,19 +47,48 @@ training <- function(true_beta, var_cov, sigma2, lambda, ntrain){
   eps <- rnorm(ntrain, mean = 0, sd = sqrt(sigma2))
   
   # Calculate Y vector 
-  Y = X %*% true_beta + eps
+  Y <- X %*% true_beta + eps
   
   
-  return(dim(Y))
+
 
   #### Calculate Beta Estimates #### 
   
+  bhat_ols <- solve(t(X) %*% X) %*% t(X) %*% Y
+  
+  bhat_ridge <- solve(t(X) %*% X + lambda*diag(nrow(var_cov))) %*% t(X) %*% Y
+  
+  
+  
+  #### Calculate Training Error #### 
+  
+  # OLS Training Error 
+  TE_ols <- (1/ntrain)*sum((Y - X %*% bhat_ols)^2)
+  
+  # Ridge Training Error 
+  TE_ridge <- (1/ntrain)*sum((Y - X %*% bhat_ridge)^2)
+  
+  
+  train_output <- list("bhat_ols" = bhat_ols, "bhat_ridge" = bhat_ridge, "X_mat" = X, "Y_mat" = Y, "TE_ols" = TE_ols, "TE_ridge" = TE_ridge)
+  
+  
+  return(train_output)
 }
 
 
 var_cov1 <- matrix(c(2, 1, 1, 2), nrow = 2) 
 bet1 <- matrix(c(4, 5), nrow = 2)
 
-training(bet1, var_cov1, 1, 1, 100)
+asdf <- training(bet1, var_cov1, 1, 1, 100)
+
+
+# Y1 <- asdf$Y_mat 
+# X1 <- asdf$X_mat
+# b_ols <- asdf$bhat_ols
+# 
+# (1/100)*sum((Y1 - X1 %*% b_ols)^2)
+
+
+
 
 
